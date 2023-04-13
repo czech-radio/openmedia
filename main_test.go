bd94b075b94779c3b68d28479ea6a8931073a295
David Landa <david.landa@protonmail.com>
2023-04-13 12:18:21 +0200

    Change test paths

--- main_test.go
+++ main_test.go
@@ -8,8 +8,8 @@ import (
 // TestProcessFolder should cover most of the code
 func TestProcessFolder(t *testing.T) {
 
-	input_path := filepath.Join("test")
-	output_path := filepath.Join("test")
+	input_path := filepath.Join("data")
+	output_path := filepath.Join("data")
 
 	err := ProcessFolder(input_path, output_path)
 	if err != nil {
