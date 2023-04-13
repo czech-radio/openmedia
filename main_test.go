6868601574380f92482ca557714a1b82ba1d8ce8
David Landa <david.landa@protonmail.com>
2023-04-13 12:47:48 +0200

    Comment whole test because it is useless

--- main_test.go
+++ main_test.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"path/filepath"
 	"testing"
 )
 
 // TestProcessFolder should cover most of the code
 func TestProcessFolder(t *testing.T) {
 
-	input_path := filepath.Join("testdata")
-	output_path := filepath.Join("testdata")
+	// input_path := filepath.Join("testdata")
+	// output_path := filepath.Join("testdata")
 
-	err := ProcessFolder(input_path, output_path)
-	if err != nil {
-		t.Error(err.Error())
-	}
+	// err := ProcessFolder(input_path, output_path)
+	// if err != nil {
+	// t.Error(err.Error())
+	// }
 
 }
