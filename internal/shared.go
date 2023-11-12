8e3e5bf7c4c13db1442639bb8921c851d5a24179
Jan Kacaba <jan.kacaba@gmail.com>
2023-11-12 18:57:29 +0100

    add TestMain test setup: create dir in ram and copy testdata, and cleanup

--- internal/shared.go
+++ internal/shared.go
@@ -3,12 +3,32 @@ package internal
 
 import (
 	"fmt"
+	"io"
 	"log/slog"
 	"os"
+	"path/filepath"
 	"runtime"
 	"strconv"
 	"time"
 )
+
+func TraceFunctionLevel(lv int) string {
+	pc := make([]uintptr, 10) // at least 1 entry needed
+	runtime.Callers(lv, pc)
+	f := runtime.FuncForPC(pc[lv-1])
+	return f.Name()
+	// file, line := f.FileLine(pc[0])
+}
+
+// TracePrint print file, function name, line in code where this function is called (skip=0: file where this function is defined, skip=1 where the function is called)
+func TracePrint(skip int) {
+	pc, fn, line, ok := runtime.Caller(skip)
+	if !ok {
+		fmt.Printf("Cannot trace function")
+		return
+	}
+	fmt.Printf("\nFile: %s\nFunc: %s:%d\n", fn, runtime.FuncForPC(pc).Name(), line)
+}
 
 // SetLogLevel: sets log level, default=0
 func SetLogLevel(level string) {
@@ -47,7 +67,7 @@ func DetectLinuxSytemOrPanic() {
 	}
 }
 
-func DirectoryIsReadable(filepath string) {
+func DirectoryIsReadableOrPanic(filepath string) {
 	// fileInfo, err := os.Stat(filepath)
 	_, err := os.Stat(filepath)
 	//handle error
@@ -73,4 +93,66 @@ func DirectoryDelete(directory string) {
 	} else {
 		panic(err)
 	}
+}
+
+func DirectoryFileList(file_path string) {
+	dirs, err := os.ReadDir(file_path)
+	if err != nil {
+		slog.Error(err.Error())
+	}
+	for _, dir := range dirs {
+		fmt.Println(dir.Name(), dir.Type(), dir.Type().IsRegular())
+	}
+}
+
+func CopyFile(src_file_path, dst_file_path string) error {
+	slog.Debug(
+		"copying file",
+		"source_file", src_file_path,
+		"dst_file", dst_file_path,
+	)
+	srcFile, err := os.Open(src_file_path)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	// Create the destination file in the destination directory for writing
+	dstFile, err := os.Create(dst_file_path)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+	// Copy the contents of the source file to the destination file
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func DirectoryCopyNoRecurse(src_dir, dst_dir string) (int, error) {
+	files_count := 0 // number of files copied
+	// Create the destination directory if it doesn't exist
+	if err := os.MkdirAll(dst_dir, 0700); err != nil {
+		return files_count, err
+	}
+	entries, err := os.ReadDir(src_dir)
+	if err != nil {
+		return files_count, err
+	}
+
+	for _, entry := range entries {
+		if entry.Type().IsRegular() {
+			srcFilePath := filepath.Join(src_dir, entry.Name())
+			dstFilePath := filepath.Join(dst_dir, entry.Name())
+			err = CopyFile(srcFilePath, dstFilePath)
+			if err != nil {
+				return files_count, err
+			}
+			files_count++
+		}
+	}
+	return files_count, nil
 }
--- internal/shared_test.go
+++ internal/shared_test.go
@@ -2,21 +2,56 @@ package internal
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 )
 
+var TEMP_DIR string          // Temporary directory inside /dev/shm created for test source files and output files
+var TEMP_DIR_TEST_SRC string // Temporary direcotory which serves as source data for tests
+var TEMP_DIR_TEST_DST string // Temporary direcotory which serves as destination for tests outputs
+var TEST_DATA_DIR_SRC string // Test data which will be copied to TEMP_DIR
+
 // TestMain setup, run tests, and teadrdown (cleanup after tests)
 func TestMain(m *testing.M) {
-	// Tests setup
+	// TESTS SETUP
 	DetectLinuxSytemOrPanic()
+	//// Setup logging
 	level := os.Getenv("GOLOGLEVEL")
 	SetLogLevel(level)
-	temp_directory := DirectoryCreateInRam()
-	// Run tests
-	code := m.Run()
-	// Testst teardown
-	DirectoryDelete(temp_directory)
-	defer os.Exit(code)
+	//// Setup testing data
+	current_directory, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	TEST_DATA_DIR_SRC = current_directory + "/../test/testdata"
+	DirectoryIsReadableOrPanic(TEST_DATA_DIR_SRC)
+	TEMP_DIR = DirectoryCreateInRam()
+	TEMP_DIR_TEST_SRC = filepath.Join(TEMP_DIR, "SRC")
+	TEMP_DIR_TEST_DST = filepath.Join(TEMP_DIR, "DST")
+	_, err = DirectoryCopyNoRecurse(
+		TEST_DATA_DIR_SRC,
+		TEMP_DIR_TEST_DST,
+	)
+
+	// RUN TESTS
+	if err == nil {
+		code := m.Run()
+		defer os.Exit(code)
+	}
+	if err != nil {
+		defer os.Exit(-1)
+	}
+
+	// TEARDOWN
+	DirectoryDelete(TEMP_DIR)
+}
+
+func TestCurrentDir(t *testing.T) {
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Log(err)
+	}
+	t.Log(dir)
 }
 
 func Test_DetectLinuxSystemPanic(t *testing.T) {
@@ -26,4 +61,18 @@ func Test_DetectLinuxSystemPanic(t *testing.T) {
 func Test_DirectoryCreateInRam(t *testing.T) {
 	directory := DirectoryCreateInRam()
 	defer os.RemoveAll(directory)
+}
+
+func TestDirectoryFileList(t *testing.T) {
+	DirectoryFileList("/tmp/")
+}
+
+func TestDirectoryCopy(t *testing.T) {
+	_, err := DirectoryCopyNoRecurse(
+		TEST_DATA_DIR_SRC,
+		TEMP_DIR+"/test_copy",
+	)
+	if err != nil {
+		t.Error(err)
+	}
 }
