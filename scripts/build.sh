1a5ef985e975e20f91deab8cc4e93ccfa61ab6af
Jan Kacaba <jan.kacaba@gmail.com>
2024-01-23 00:48:03 +0100

    release create

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
 # Build the Go binary with linked variables.
-#NOTE: -ldflags can modify directly only simple variables i.e. it cannot directly modify struct fields.
+#NOTE: -ldflags can modify directly only simple variables i.e. it cannot directly modify struct instance fields.
 BUILD_TIME="$(date +%FT%T)"
 GIT_COMMIT="$(git rev-parse HEAD)"
 GIT_TAG="$(git describe --tags --abbrev=0)"
-GOSRC_PATH="github/triopium/github_workflows/golang_dummy/cmd"
+GOSRC_PATH="github/openmedia-archive/cmd"
 BINARY_NAME="${PWD##*/}"
 
 declare -a GOSRC_VAR=(
