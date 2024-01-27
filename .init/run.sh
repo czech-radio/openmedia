ecb5c895b3d502fc8861b21917275d7cbb411a9a
Jan Kacaba <jan.kacaba@gmail.com>
2024-01-27 19:55:34 +0100

    update deploy/init

--- .init/run.sh
+++ .init/run.sh
@@ -34,65 +34,59 @@ GetNewReleaseTag(){
   curl --silent "${REPO_URL}/releases/latest" | jq -r ".tag_name"
 }
 
-DownloadAsset(){
-  local tag="$1"
-  local asset="$2"
-  local assets_url="${ASSET_DOWNLOAD_URL}/${tag}"
-  if ! curl --silent -L -O "${assets_url}/${asset}" ; then
-    # --clobber: overwrite destination files
-    echo "Failed to download new version assets: $tag" >&2
-    return 1
+DownloadReleaseFile(){
+  local release_tag="$1"
+  local filename="$2"
+  local filename_url="$ASSET_DOWNLOAD_URL/$release_tag/$filename"
+  if curl --silent -L -O "${filename_url}" ; then
+    echo "Failed to download release file: $release_tag" >&2
+    rm "$filename"
   fi
-  echo "Download new version asset: ${tag}/${asset}"
+  echo "Download release file: ${tag}/${asset}"
 }
 
-DownloadTagReleaseFiles(){
-  local tag="$1"
-  local assets_url="${ASSET_DOWNLOAD_URL}/${tag}"  
-  echo "Downloading assets"
-  DownloadAsset "$tag" "${BINARY_NAME}"
-  DownloadAsset "$tag" "${BINARY_NAME}.service"
-  DownloadAsset "$tag" "${BINARY_NAME}.timer"
-  DownloadAsset "$tag" "version.txt"
-  # DownloadAsset "$tag" "run.sh"
-  # chmod u+x "./run.sh"
-  chmod u+x "./${BINARY_NAME}"
-}
-
-ServiceUpdate(){
-  #TODO: graceful handling of deactivation of running service: when the main command is still running. e.g. through service unit file directives. Trap errors log.
-  local tag="$1"
-  if [[ "$AUTO_UPDATE_SERVICE" != "true" ]]; then
+NeedsUpdate(){
+  local latest_tag="$(GetNewReleaseTag)"
+  local latest_date="$(curl -s -L "https://github.com/czech-radio/openmedia-archive/releases/download/${latest_tag}/version.txt" | head -1)"
+  local current_date="$(head -1 version.txt)"
+  if [[ "$current_date" == "$latest_date" ]] && "${AUTO_UPDATE_SERVICE}" ; then
+    echo true
     return
   fi
-  # local latest_tag="$(curl -s -L https://github.com/czech-radio/openmedia-archive/releases/download/v0.9.0/version.txt | yq '.tag')"
-  # local current_tag=$(cat version.txt | yq '.tag')
-  local latest_date="$(curl -s -L https://github.com/czech-radio/openmedia-archive/releases/download/v0.9.0/version.txt | yq '.date')"
-  local current_date=$(cat version.txt | yq '.date')
- 
-  # if [[ "$current_tag" != "$latest_tag" ]]; then
-  if [[ "$current_date" != "$latest_date" ]]; then
-    echo Updating service assets
-    DownloadTagReleaseFiles "$latest_tag"
-    systemctl --user daemon-reload
-  fi
+  echo false
+}
+
+DownloadReleaseFiles(){
+  local release_tag="$1"
+  declare -a ReleaseFiles=(
+    "${BINARY_NAME}"
+    "${BINARY_NAME}.service"
+    "${BINARY_NAME}.timer"
+    "version.txt"
+  )
+  echo "Downloading assets"
+
+  update="$(NeedsUpdate)"
+  for file in "${ReleaseFiles[@]}"; do
+    if [[ ! -f $filename ]] || "$update" ; then
+      DownloadReleaseFile "$release_tag" "$file"
+    fi
+  done
+  chmod u+x "./${BINARY_NAME}"
+  systemctl --user daemon-reload
 }
 
 ServiceServe(){
-  local tag="$RELEASE_TAG"
-  if [[ "$tag" == "latest" ]] ; then
+  #TODO: graceful handling of deactivation of running service: when the main command is still running. e.g. through service unit file directives. Trap errors log.
+  local release_tag="$RELEASE_TAG"
+  if [[ "$relese_tag" == "latest" ]] ; then
     tag="$(GetNewReleaseTag)"
   fi
-  if [[ ! "$tag" =~ v.* ]] ; then
+  if [[ ! "$relese_tag" =~ v.* ]] ; then
     echo "Cannot get tag name. Asset files not downloaded." >&2
     return 1
   fi
-  
-  # Check if assets are present
-  if [[ ! -f "version.txt" ]]; then
-    DownloadTagReleaseFiles "$tag"
-  fi
-  ServiceUpdate "$tag"
+  DownloadReleaseFiles "$release_tag"
   
   # Activate service
   service_status="$(systemctl --user is-enabled "$SERVICE_NAME")"
