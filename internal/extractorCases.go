31eb8139e09476912869795a80ce7097f5aa1b9b
Jan Kacaba <jan.kacaba@gmail.com>
2024-03-22 22:50:14 +0100

    cleanup

--- internal/extractorCases.go
+++ internal/extractorCases.go
@@ -32,6 +32,23 @@ var ProductionFieldsRadioStory = []string{
 	"5070",
 }
 
+var ProductionFieldsAudio = []string{
+	"8",
+	"1005",
+	"5082",
+}
+
+var ProductionFieldsContactItems = []string{
+	"421",
+	"422",
+	"423",
+	"424",
+	"5015",
+	"5016",
+	"5087",
+	"5088",
+}
+
 var HeaderFieldPath string = "/OM_HEADER/OM_FIELD"
 var RecordFieldPath string = "/OM_FIELD"
 
@@ -49,8 +66,7 @@ var EXTproduction = OMextractors{
 		FieldsPath:       HeaderFieldPath,
 		FieldIDs:         ProductionFieldsSubRundown,
 		PartPrefixCode:   FieldPrefix_SubHead,
-		// KeepInputRows:    false,
-		KeepInputRows: true,
+		KeepInputRows:    true,
 	},
 	{
 		ObjectPath:       "/<OM_RECORD>/Radio Story",
@@ -61,284 +77,39 @@ var EXTproduction = OMextractors{
 		KeepInputRows:    false,
 	},
 	// {
-	// ObjectPath: "/*Audioclip",
-	// ObjectPath:    "/<OM_RECORD>/Contact Item",
-	// FieldsPath:    "/OM_HEADER/OM_FIELD",
-	// FieldIDs:      []string{"8", "1005", "5082"},
-	// FieldsPrefix:  "Aud-HED",
-	// KeepInputRows: true,
-	// KeepInputRows: false,
+	// ObjectPath:     "<OM_RECORD>/Audioclip",
+	// FieldsPath:     "/OM_HEADER/OM_FIELD",
+	// FieldIDs:       ProductionFieldsAudio,
+	// PartPrefixCode: FieldPrefix_AudioClipHead,
+	// KeepInputRows:  true,
 	// },
-	// {
-	// ObjectPath: "/<OM_RECORD>/Audioclip",
-	// ObjectPath:    "/<OM_RECORD>/Contact Item",
-	// FieldsPath:    "/OM_HEADER/OM_FIELD",
-	// FieldIDs:      []string{"8", "1005", "5082"},
-	// FieldsPrefix:  "Cont-HED",
-	// KeepInputRows: true,
-	// KeepInputRows: false,
-	// },
-	// {
-	// ObjectPath: "/<OM_RECORD>/Contact Item",
-	// FieldsPath: "/OM_HEADER/OM_FIELD",
-	// FieldIDs: []string{
-	// "421", "422", "423", "424", "5015", "5016", "5087", "5088"},
-	// FieldsPrefix:  "Contact-HED",
-	// KeepInputRows: true,
-	// },
-
-	// {
-	// ObjectPath:   "/*Sub Rundown",
-	// FieldsPath:   "/OM_HEADER/OM_FIELD",
-	// FieldIDs:     []string{"8", "1004", "1003", "1005", "321"},
-	// FieldsPrefix: "SubR-HED",
-	// },
-	// {
-	// ObjectPath: "<OM_RECORD>/Radio Story",
-	// FieldsPath: "OM_HEADER",
-	// FieldIDs: []string{
-	// "8", "5081", "1004", "1004", "1003", "1005", "1035", "1036",
-	// "1029", "1010", "1002", "321", "5079", "16", "5082", "5072", "5016"
-	// },
-	// FieldsPrefix: "Story-HED",
-	// },
+	{
+		ObjectPath:     "<OM_RECORD>/Contact Item",
+		FieldsPath:     "/OM_HEADER/OM_FIELD",
+		FieldIDs:       ProductionFieldsContactItems,
+		PartPrefixCode: FieldPrefix_ContactItemHead,
+		KeepInputRows:  true,
+	},
 }
 
-// var EXTproductionRECandHED = OMextractors{
-// 	{
-// 		ObjectPath:   "/Radio Rundown",
-// 		FieldsPath:   "/OM_HEADER/OM_FIELD",
-// 		FieldIDs:     []string{"5081", "1", "8", "1003", "1004"},
-// 		FieldsPrefix: "RadioR-HED",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>",
-// 		FieldsPath:   "/OM_FIELD",
-// 		FieldIDs:     []string{"1", "8"},
-// 		FieldsPrefix: "RadioR-REC",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown",
-// 		FieldsPath:   "/OM_HEADER/OM_FIELD",
-// 		FieldIDs:     []string{"1", "8"},
-// 		FieldsPrefix: "HourlyR-HED",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>",
-// 		FieldsPath:   "/OM_FIELD",
-// 		FieldIDs:     []string{"8"},
-// 		FieldsPrefix: "HourlyR-REC",
-// 	},
-// 	{
-// 		ObjectPath: "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown",
-// 		FieldsPath: "/OM_HEADER/OM_FIELD",
-// 		FieldIDs: []string{
-// 			"5001", "8", "1004", "1003", "1005", "321", "5079"},
-// 		FieldsPrefix: "SubR-HED",
-// 	},
-// 	{
-// 		ObjectPath: "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>",
-// 		FieldsPath: "/OM_FIELD",
-// 		FieldIDs: []string{
-// 			"5001", "8", "1004", "1003", "1005", "321", "5079"},
-// 		FieldsPrefix: "Sub-REC",
-// 	},
-// 	{
-// 		ObjectPath: "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>/Radio Story",
-// 		FieldsPath: "/OM_HEADER/OM_FIELD",
-// 		FieldIDs: []string{
-// 			"5001", "8", "5081", "1004", "1003", "1005", "1036", "1029", "1010", "1002", "321", "5079", "16", "5082", "5072", "5016", "5", "6", "12", "5071", "5070"},
-// 		FieldsPrefix: "RStory-HED",
-// 	},
-// 	{
-// 		ObjectPath: "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>/Radio Story/<OM_RECORD>",
-// 		FieldsPath: "/OM_FIELD",
-// 		FieldIDs: []string{
-// 			"5001", "8", "5081", "1004", "1003", "1005", "1036", "1029", "1010", "1002", "321", "5079", "16", "5082", "5072", "5016", "5", "6", "12", "5071", "5070"},
-// 		FieldsPrefix: "RStory-REC",
-// 	},
-// 	// {
-// 	// ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>/Radio Story",
-// 	// FieldsPath:   "/OM_HEADER/OM_FIELD",
-// 	// FieldIDs:     []string{"8"},
-// 	// FieldsPrefix: "RStory-HED",
-// 	// },
-// 	// {
-// 	// ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Radio Story/Contact Item",
-// 	// FieldsPath:   "/OM_HEADER/OM_FIELD",
-// 	// FieldIDs:     []string{"8", "10"},
-// 	// FieldsPrefix: "CI-HED",
-// 	// },
-// }
+//"/Radio Rundown",
+//"/Radio Rundown/<OM_RECORD>",
+//"/Radio Rundown/<OM_RECORD>/Hourly Rundown",
+//"/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>",
+//"/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown",
+//"/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>",
+//"/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>/Radio Story",
+//"/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>/Radio Story/<OM_RECORD>",
+//"/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>/Radio Story",
+//"/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Radio Story/Contact Item",
 
-// var EXTproductionTest = OMextractors{
-// 	{
-// 		ObjectPath:   "/Radio Rundown",
-// 		FieldsPath:   "/OM_HEADER/OM_FIELD",
-// 		FieldIDs:     []string{"5081", "1", "8", "1003", "1004"},
-// 		FieldsPrefix: "RadioR-HED",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>",
-// 		FieldsPath:   "/OM_FIELD",
-// 		FieldIDs:     []string{"1", "8"},
-// 		FieldsPrefix: "RadioR-REC",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown",
-// 		FieldsPath:   "/OM_HEADER/OM_FIELD",
-// 		FieldIDs:     []string{"1", "8"},
-// 		FieldsPrefix: "HourlyR-HED",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>",
-// 		FieldsPath:   "/OM_FIELD",
-// 		FieldIDs:     []string{"8"},
-// 		FieldsPrefix: "HourlyR-REC",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown",
-// 		FieldsPath:   "/OM_HEADER/OM_FIELD",
-// 		FieldIDs:     []string{"5001", "8", "1004", "1003", "1005", "321", "5079"},
-// 		FieldsPrefix: "SubR-HED",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>",
-// 		FieldsPath:   "/OM_FIELD",
-// 		FieldIDs:     []string{"5001", "8", "1004", "1003", "1005", "321", "5079"},
-// 		FieldsPrefix: "Sub-REC",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>/Radio Story",
-// 		FieldsPath:   "/OM_HEADER/OM_FIELD",
-// 		FieldIDs:     []string{"5001", "8", "5081", "1004", "1003", "1005", "1036", "1029", "1010", "1002", "321", "5079", "16", "5082", "5072", "5016", "5", "6", "12", "5071", "5070"},
-// 		FieldsPrefix: "RStory-HED",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>/Radio Story/<OM_RECORD>",
-// 		FieldsPath:   "/OM_FIELD",
-// 		FieldIDs:     []string{"5001", "8", "5081", "1004", "1003", "1005", "1036", "1029", "1010", "1002", "321", "5079", "16", "5082", "5072", "5016", "5", "6", "12", "5071", "5070"},
-// 		FieldsPrefix: "RStory-REC",
-// 	},
-// 	// {
-// 	// ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>/Radio Story",
-// 	// FieldsPath:   "/OM_HEADER/OM_FIELD",
-// 	// FieldIDs:     []string{"8"},
-// 	// FieldsPrefix: "RStory-HED",
-// 	// },
-// 	// {
-// 	// ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Radio Story/Contact Item",
-// 	// FieldsPath:   "/OM_HEADER/OM_FIELD",
-// 	// FieldIDs:     []string{"8", "10"},
-// 	// FieldsPrefix: "CI-HED",
-// 	// },
-// }
-
-// var EXTproductionWorks = OMextractors{
-// 	{
-// 		ObjectPath:   "/Radio Rundown",
-// 		FieldsPath:   "/OM_HEADER/OM_FIELD",
-// 		FieldIDs:     []string{"1", "8"},
-// 		FieldsPrefix: "RR-He",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>",
-// 		FieldsPath:   "/OM_FIELD",
-// 		FieldIDs:     []string{"8"},
-// 		FieldsPrefix: "HR-Re",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown",
-// 		FieldsPath:   "/OM_HEADER/OM_FIELD",
-// 		FieldIDs:     []string{"1", "8"},
-// 		FieldsPrefix: "H-He",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>",
-// 		FieldsPath:   "/OM_FIELD",
-// 		FieldIDs:     []string{"1", "8"},
-// 		FieldsPrefix: "H-Re",
-// 	},
-// 	// {
-// 	// ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown",
-// 	// FieldsPath:   "OM_HEADER/OM_FIELD",
-// 	// FieldIDs:     []string{"1", "2", "3", "4"},
-// 	// FieldsPrefix: "S-He",
-// 	// FieldIDs:     []string{"*"}, -> No ID, NO VALUE
-// 	// },
-// }
-
-// var EXTall = OMextractors{
-// 	{
-// 		ObjectPath:   "/Radio Rundown",
-// 		FieldsPath:   "/OM_HEADER/OM_FIELD",
-// 		FieldIDs:     []string{"*"},
-// 		FieldsPrefix: "R-He",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>",
-// 		FieldsPath:   "/OM_FIELD",
-// 		FieldsPrefix: "R-Re",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown",
-// 		FieldsPath:   "/OM_HEADER/OM_FIELD",
-// 		FieldsPrefix: "H-He",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>",
-// 		FieldsPath:   "/OM_FIELD",
-// 		FieldsPrefix: "H-Re",
-// 	},
-// 	{
-// 		ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown",
-// 		FieldsPath:   "OM_HEADER/OM_FIELD",
-// 		FieldsPrefix: "S-He",
-// 	},
-// 	// {
-// 	// ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>",
-// 	// FieldsPath:   "OM_FIELD",
-// 	// FieldIDs:     []string{"1", "8"},
-// 	// FieldsPrefix: "S-He",
-// 	// },
-
-// 	// VAR1
-// 	// {
-// 	// ObjectPath:   "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>/Radio Story",
-// 	// FieldsPath:   "OM_HEADER/OM_FIELD",
-// 	// FieldIDs:     []string{"1", "8"},
-// 	// FieldsPrefix: "S-He",
-// 	// },
-
-// 	// VAR2
-// 	// {
-// 	// ObjectPath:   "/Radio Story",
-// 	// FieldsPath:   "OM_HEADER/OM_FIELD",
-// 	// FieldIDs:     []string{"1", "8"},
-// 	// FieldsPrefix: "S-He",
-// 	// },
-// 	// {
-// 	// ObjectPath:   "/Radio Story/<OM_RECORD>",
-// 	// FieldsPath:   "OM_HEADER/OM_FIELD",
-// 	// FieldIDs:     []string{"1", "8"},
-// 	// FieldsPrefix: "S-He",
-// 	// },
-// 	// {
-// 	// ObjectPath:   "/Radio Story/<OM_RECORD>/Contact Item",
-// 	// FieldsPath:   "OM_HEADER/OM_FIELD",
-// 	// FieldIDs:     []string{"1", "8"},
-// 	// FieldsPrefix: "S-He",
-// 	// },
-// 	// ObjectPath:   "/Radio Story/<OM_RECORD>/AudioClip",
-// 	// FieldsPath:   "OM_HEADER/OM_FIELD",
-// 	// FieldIDs:     []string{"1", "8"},
-// 	// FieldsPrefix: "S-He",
-// 	// },
-// }
-
-// // OmObject:   "Radio Story",
-// // ObjectPath: "//AudioClip",
-// // OmObject:   "Radio Story",
-// // ObjectPath: "//Contact Item",
-
-// // "//OM_OBJECT[@TemplateName='%s']/%s/*", ext.OM_type, ext.Path,
+//"/Radio Rundown",
+//"/Radio Rundown/<OM_RECORD>",
+//"/Radio Rundown/<OM_RECORD>/Hourly Rundown",
+//"/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>",
+//"/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown",
+//"/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>",
+//"/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>/Radio Story",
+//"/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>/Radio Story/<OM_RECORD>",
+// "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Sub Rundown/<OM_RECORD>/Radio Story",
+// "/Radio Rundown/<OM_RECORD>/Hourly Rundown/<OM_RECORD>/Radio Story/Contact Item",
