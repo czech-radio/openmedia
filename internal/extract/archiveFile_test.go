1683323f1a85747a8d24276add840ecf9fc27ad9
Jan Kacaba <jan.kacaba@gmail.com>
2024-04-15 09:12:33 +0200

    removed test local path

--- internal/extract/archiveFile_test.go
+++ internal/extract/archiveFile_test.go
@@ -1,13 +1,9 @@
 package extract
 
 import (
-	"fmt"
-	ar "github/czech-radio/openmedia-archive/internal/archive"
 	"github/czech-radio/openmedia-archive/internal/helper"
 	"log/slog"
 	"testing"
-
-	"github.com/antchfx/xmlquery"
 )
 
 var testerConfig = helper.TesterConfig{
@@ -22,23 +18,23 @@ func TestMain(m *testing.M) {
 	testerConfig.CleanuUP()
 }
 
-func TestXMLqueryFile(t *testing.T) {
+// func TestXMLqueryFile(t *testing.T) {
 
-	filePath := "/home/jk/CRO/CRO_BASE/openmedia-archive_backup/Archive/control/control_UTF16_RD_13-17_Plus_Tuesday_W01_2024_01_02.xml"
-	af := ArchiveFile{}
-	err := af.Init(ar.WorkerTypeRundownXMLutf16le, filePath)
-	if err != nil {
-		t.Error(err.Error())
-	}
-	// query := "OM_OBJECT[@TemplateName='Radio Rundown']/OM_RECORD/OM_OBJECT[@TemplateName='Hourly Rundown']/OM_RECORD/OM_OBJECT[@TemplateName='Radio Story']"
-	queries := map[string]string{
-		"story":       "//OM_OBJECT[@TemplateName='Radio Story']",  // 130
-		"audio":       "//OM_OBJECT[@TemplateName='Audioclip']",    // 84
-		"contact":     "//OM_OBJECT[@TemplateName='Contact Item']", // 66
-		"audo+contac": "//OM_OBJECT[@TemplateName='Audioclip' or @TemplateName='Contact Item']",
-	}
-	for i, q := range queries {
-		subNodes := xmlquery.Find(af.BaseNode, q)
-		fmt.Println(i, q, len(subNodes))
-	}
-}
+// 	filePath := "/home/jk/CRO/CRO_BASE/openmedia-archive_backup/Archive/control/control_UTF16_RD_13-17_Plus_Tuesday_W01_2024_01_02.xml"
+// 	af := ArchiveFile{}
+// 	err := af.Init(ar.WorkerTypeRundownXMLutf16le, filePath)
+// 	if err != nil {
+// 		t.Error(err.Error())
+// 	}
+// 	// query := "OM_OBJECT[@TemplateName='Radio Rundown']/OM_RECORD/OM_OBJECT[@TemplateName='Hourly Rundown']/OM_RECORD/OM_OBJECT[@TemplateName='Radio Story']"
+// 	queries := map[string]string{
+// 		"story":       "//OM_OBJECT[@TemplateName='Radio Story']",  // 130
+// 		"audio":       "//OM_OBJECT[@TemplateName='Audioclip']",    // 84
+// 		"contact":     "//OM_OBJECT[@TemplateName='Contact Item']", // 66
+// 		"audo+contac": "//OM_OBJECT[@TemplateName='Audioclip' or @TemplateName='Contact Item']",
+// 	}
+// 	for i, q := range queries {
+// 		subNodes := xmlquery.Find(af.BaseNode, q)
+// 		fmt.Println(i, q, len(subNodes))
+// 	}
+// }
