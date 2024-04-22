7ff97d177509395d0fe70f0f2daf3d25de457a68
Jan Kacaba <jan.kacaba@gmail.com>
2024-04-22 19:14:51 +0200

    testig why github fails cmd test

--- cmd/archive_test.go
+++ cmd/archive_test.go
@@ -2,33 +2,32 @@ package cmd
 
 import (
 	"fmt"
-	"github/czech-radio/openmedia/internal/helper"
 	"os/exec"
 	"testing"
 )
 
-var testerConfig = helper.TesterConfig{
-	TempDirName:    "openmedia",
-	TestDataSource: "../test/testdata",
-}
+// var testerConfig = helper.TesterConfig{
+// 	TempDirName:    "openmedia",
+// 	TestDataSource: "../test/testdata",
+// }
 
-func TestMain(m *testing.M) {
-	testerConfig.TesterMain(m)
-}
+// func TestMain(m *testing.M) {
+// 	testerConfig.TesterMain(m)
+// }
 
 func TestCmdArchive(t *testing.T) {
-	testSubdir := "cmd"
-	defer testerConfig.RecoverPanic(t)
-	testerConfig.InitTest(t, testSubdir)
+	// testSubdir := "cmd"
+	// defer testerConfig.RecoverPanic(t)
+	// testerConfig.InitTest(t, testSubdir)
 	// tpd := testerConfig.TempDestinationPathGeter(testSubdir)
 	// dstFile := tpd("openmedia")
-	testerConfig.WaitAdd()
+	// testerConfig.WaitAdd()
 	cmd := exec.Command("go", "run", "../main.go")
 	res, err := cmd.CombinedOutput()
-	fmt.Println(string(res))
 	if err != nil {
-		panic(err)
-		// t.Error(err)
+		// panic(err)
+		t.Error(err)
 	}
-	testerConfig.WaitDone()
+	fmt.Println(string(res))
+	// testerConfig.WaitDone()
 }
