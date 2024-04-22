9f95804b90ceabd7328c76f389770dae5a3b9a94
Jan Kacaba <jan.kacaba@gmail.com>
2024-04-22 10:41:42 +0200

    add xml get base node and level up in xml tree functions

--- internal/helper/xml.go
+++ internal/helper/xml.go
@@ -1,23 +1,39 @@
 package helper
 
 import (
+	"fmt"
+	"io"
+
 	"github.com/antchfx/xmlquery"
 )
 
-/**
- * Function: NodeGetParent___
- *
- * @param node:    ___
- * @param levelUp: ___
- * @return: [*xmlquery.Node, bool] ___
- */
-func NodeGetParent(node *xmlquery.Node, levelUp int) (*xmlquery.Node, bool) {
+// NodeGetParent go up in xmltree acording to level. If level greater than top most node, return top most node
+func NodeGetParent(node *xmlquery.Node, levelUp int) (*xmlquery.Node, int) {
+	var levelUpCount int
 	resultNode := node
 	if levelUp == 0 {
-		return resultNode, true
+		return resultNode, levelUpCount
 	}
-	for i := 0; i <= levelUp; i++ {
-		resultNode = resultNode.Parent
+	for i := 0; i < levelUp; i++ {
+		subRes := resultNode.Parent
+		fmt.Printf("%d: %+v\n", i, subRes)
+		if subRes.Parent == nil {
+			break
+		}
+		levelUpCount++
+		resultNode = subRes
 	}
-	return resultNode, true
+	return resultNode, levelUpCount
+}
+
+// XMLgetBaseNode get first significant node in xml tree
+func XMLgetBaseNode(
+	breader io.Reader) (*xmlquery.Node, error) {
+	// Parse first xml tree
+	xmlNode, err := xmlquery.Parse(breader)
+	if err != nil {
+		return nil, err
+	}
+	node := xmlNode.FirstChild.NextSibling
+	return node, nil
 }
--- internal/helper/xml_test.go
+++ internal/helper/xml_test.go
@@ -1,42 +1,56 @@
 package helper
 
+import (
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/antchfx/xmlquery"
+)
+
 var ExampleXmlLevels = `
 <root attr1="value1">
-    <level1 attr2="value2">
-        <level2 attr3="value3">
-            <level3 attr4="value4">
-                <level4 attr5="value5">Content of level 4</level4>
-            </level3>
-        </level2>
-    </level1>
+	<level1 attr2="value2">
+		<level2 attr3="value3">
+			<level3 attr4="value4">
+				<level4 attr5="value5">Content of level 4</level4>
+			</level3>
+		</level2>
+  </level1>
 </root>
 `
 
-// func TestNodeGetParent(t *testing.T) {
-// 	reader := strings.NewReader(ExampleXmlLevels)
-// 	baseNode, _ := xmlquery.Parse(reader)
-// 	type args struct {
-// 		node    *xmlquery.Node
-// 		levelUp int
-// 	}
-// 	tests := []struct {
-// 		name string
-// 		args args
-// 		want *xmlquery.Node
-// 	}{
-// 		{"A", args{baseNode, 0}, baseNode},
-// 		{"B", args{baseNode.FirstChild, 1}, baseNode},
-// 	}
-// 	fmt.Println("kek", xmlquery.Find(baseNode, "/root"))
-// for _, tt := range tests {
-// 	t.Run(tt.name, func(t *testing.T) {
-// 		got, _ := NodeGetParent(tt.args.node, tt.args.levelUp)
-// 		ok := reflect.DeepEqual(got, tt.want)
-// 		if !ok {
-// 			t.Errorf(
-// 				"NodeGetParent() = %v, want %v",
-// 				got, tt.want)
-// 		}
-// 	})
-// }
-// }
+func TestNodeGetParent(t *testing.T) {
+	// Prepare test pairs
+	reader := strings.NewReader(ExampleXmlLevels)
+	baseNode, err := XMLgetBaseNode(reader)
+	if err != nil {
+		t.Error(err)
+	}
+	type args struct {
+		node    *xmlquery.Node
+		levelUp int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *xmlquery.Node
+	}{
+		{"up_zero_level", args{baseNode, 0}, baseNode},
+		{"up_one_level", args{baseNode.FirstChild.NextSibling, 1}, baseNode},
+		{"up_two_levels", args{baseNode.FirstChild.NextSibling.FirstChild, 2}, baseNode},
+		{"up_three_levels", args{baseNode.FirstChild.NextSibling.FirstChild.NextSibling.FirstChild, 3}, baseNode},
+		{"up_more_levels", args{baseNode.FirstChild.NextSibling.FirstChild.NextSibling.FirstChild, 10}, baseNode},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, _ := NodeGetParent(tt.args.node, tt.args.levelUp)
+			ok := reflect.DeepEqual(got, tt.want)
+			if !ok {
+				t.Errorf(
+					"NodeGetParent() = %v, want %v",
+					got, tt.want)
+			}
+		})
+	}
+}
