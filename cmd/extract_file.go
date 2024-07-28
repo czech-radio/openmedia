e0dcee36cd5b6da60878887460ef04758c0a1e5a
Jan Kacaba <jan.kacaba@gmail.com>
2024-07-28 19:54:42 +0200

    Simplified and extended extract specified rundown file (utf8,utf16)

--- cmd/extract_file.go
+++ cmd/extract_file.go
@@ -3,51 +3,47 @@ package cmd
 import (
 	"fmt"
 	"github/czech-radio/openmedia/internal/extract"
-
-	c "github.com/triopium/go_utils/pkg/configure"
-	"github.com/triopium/go_utils/pkg/helper"
 )
 
 func commandExtractFileConfigure() {
-	add := SubcommandConfig.AddOption
-	add("SourceFilePath", "sfp",
-		"", "string", c.NotNil,
-		"Source rundown file.", nil, helper.FileExists)
-	add("OutputDirectory", "odir",
-		"", "string", c.NotNil,
-		"Output file path for extracted data.", nil,
-		helper.DirectoryExists)
-	add("OutputFileName", "ofn",
-		"", "string", c.NotNil,
-		"Output file path for extracted data.", nil,
-		nil)
-	// helper.File)
-	// add("SourceFileEncoding", "sfe", "UTF8", "string", "",
-	// 	"Source file encoding. Original files has UTF16le encoding. Minified files has UTF8 encoding.",
-	// 	[]string{"UTF8", "UTF16le"}, nil)
-	CommonExtractOptions()
+	OptionsCommonExtractPath()
+	OptionsCommonExtractFilter()
 }
 
 func (gc GlobalConfig) RunCommandExtractFile() {
 	af := extract.ArchiveFile{}
 	commandExtractFileConfigure()
-	SubcommandConfig.SubcommandOptionsParse(&af)
-	fmt.Printf("%+v\n", af)
+	SubcommandConfig.SubcommandOptionsParse(&af.ArchiveQueryCommon)
+	SubcommandConfig.SubcommandOptionsParse(&af.ArchiveIO)
+	SubcommandConfig.SubcommandOptionsParse(&af.FilterFile)
+	fmt.Printf("%+v\n", af.ArchiveQueryCommon)
+	fmt.Printf("%+v\n", af.ArchiveIO)
+	fmt.Printf("%+v\n", af.FilterFile)
 	err := af.Init()
 	if err != nil {
 		panic(err)
 	}
-
-	extCode := extract.ExtractorsPresetCode(af.ExtractorsName)
-	extractors, ok := extract.ExtractorsCodeMap[extCode]
+	exCode := af.ArchiveQueryCommon.ExtractorsCode
+	extractors, ok := extract.ExtractorsCodeMap[exCode]
 	if !ok {
-		panic(fmt.Errorf("extractors name not defined: %s", extCode))
+		panic(fmt.Errorf("extractors name not defined: %s", exCode))
 	}
 	err = af.ExtractByXMLquery(extractors)
 	if err != nil {
 		panic(err)
 	}
-	// queryOpts := ParseConfigOptions()
-	// processName := "base"
-	// af.OutputBaseDataset(processName, queryOpts)
+	af.OutputAll(
+		&af.ArchiveQueryCommon, &af.ArchiveIO, &af.FilterFile)
 }
+
+// add("SourceFileEncoding", "sfe", "UTF8", "string", "",
+// 	"Source file encoding. Original files has UTF16le encoding. Minified files has UTF8 encoding.",
+// 	[]string{"UTF8", "UTF16le"}, nil)
+// CommonExtractOptions()
+
+// afq := extract.ArchiveQueryCommon{}
+// afio := extract.ArchiveIO{}
+// SubcommandConfig.SubcommandOptionsParse(&afq)
+// SubcommandConfig.SubcommandOptionsParse(&afio)
+// fmt.Printf("%+v\n", afq)
+// fmt.Printf("%+v\n", afio)
